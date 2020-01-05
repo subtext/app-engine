92fc2156567c1480ae5c2acdeae8279e03b219dd
Alonzo Turner <alonzo.turner@subtextproductions.com>
2020-01-04 20:16:17 -0600

    compile bootstrap and js resources

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
-const {src, dest} = require('gulp');
+const {src, dest, series, parallel} = require('gulp');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
 
-function defaultTask() {
+function buildJavaScript() {
     return src('src/js/**/*.js')
         .pipe(concat('index.min.js'))
         .pipe(babel({presets: ['@babel/preset-env']}))
         .pipe(uglify())
+        .pipe(src([
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/popper.js/dist/popper.min.js'
+        ]))
         .pipe(dest('public/js/'));
 }
 
-exports.default = defaultTask;
+function buildStyle() {
+    return src('src/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(dest('public/css/'));
+}
+
+exports.default = parallel(buildStyle, buildJavaScript);
+exports.style = buildStyle;
+
